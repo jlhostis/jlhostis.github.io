cb079385414c6cec7b4f1176902fcaf2c77f6de8
jlhostis <jlhostis@protonmail.com>
2025-09-12 14:51:53 +0200

    updated talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,7 +1,12 @@
 var addressPoints = [
   [
+    "Lille, France",
+    50.6365654,
+    3.0635282
+  ],
+  [
     "Prague, Czech Republic",
-    50.0596288,
-    14.4464593
+    50.0874654,
+    14.4212535
   ]
 ];
